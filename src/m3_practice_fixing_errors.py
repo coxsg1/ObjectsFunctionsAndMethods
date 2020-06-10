f280edef82032ace3d752a7b06f31b0abeb3405d
coxsg1 <coxsg1@rose-hulman.edu>
2020-06-10 13:10:57 -0400

    Errors fixed

--- src/m3_practice_fixing_errors.py
+++ src/m3_practice_fixing_errors.py
@@ -2,11 +2,11 @@
 This module lets you practice correcting SYNTAX (notation) errors.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Sarah Cox.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   Locate the syntax (notation) errors in this file
 #   by looking for red underlines.
 #
@@ -28,27 +28,28 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #  When complete Commit and Push your work (as always).
 #
 ########################################################################
+from cmath import cos, sin, pi
 
 import rosegraphics as rg
-import math
 
 
 def main():
     """ Calls the other functions in this module to demo them. """
     print_math()
-    tutle_fn()
+    turtle_fn()
 
 
-def print_math:
+def print_math():
     """ Prints some calculated values. """
+
     x = cos(pi)
     print(x)
 
     y = sin(pi)
-    print(The sine of PI is, y)
+    print(y)
 
 
-def turtle_fun():
+def turtle_fn():
     """
     Constructs a TurtleWindow,
     constructs a classic SimpleTurtle and asks it to do some things,
@@ -57,19 +58,21 @@ def turtle_fun():
     window = rg.TurtleWindow()
 
     alan = rg.SimpleTurtle()
-alan.pen = rg.Pen('blue', 30)
+    alan.pen = rg.Pen('blue', 30)
     alan.paint_bucket = rg.PaintBucket('yellow')
 
-    alan.back(3 * (47 + 16)
-    al.begin_fill()
-    alan.circle(25)
+    alan.backward(3 * (47 + 16))
+    alan.begin_fill()
+    alan.draw_circle(25)
     alan.end_fill()
 
-    forward(200)
+    alan.forward(200)
 
     window.close_on_mouse_click()
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # ----------------------------------------------------------------------
+
 main()
